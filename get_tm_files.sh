57af22315581bff421f835ed8844ef1cf14fb0d1
fserva <federico.serva@gmail.com>
2021-01-11 12:27:21 +0000

    added script to retrieve required TM files

---
+++ get_tm_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Files from https://thematicmapping.org/downloads/world_borders.php
+# are required to build neyCDF file with countries. 
+
+wget https://thematicmapping.org/downloads/TM_WORLD_BORDERS-0.3.zip
+
